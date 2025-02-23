ed724a79ca6e8538e10e52be6b36cdfa13458b49
Sudhanshu <122535400+Suk022@users.noreply.github.com>
2025-02-24 02:15:34 +0530

    Java OOPS Basics

---
+++ day10/employeeCLASS.java
@@ -0,0 +1,14 @@
+package day10;
+
+public class employeeCLASS {
+    int empId;
+    String empName;
+    String job;
+    int sal;
+
+    void display(){
+        System.out.println("Here is the information of the Employee: ");
+        System.out.println("Employee's ID: `"+empId+"`, Employee's Name: `"+empName+"`, Job of the Employee: `"+job+"`, Employee's Salary: `"+sal+"`.");        
+    }
+
+}
